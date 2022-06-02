9c8e5a281ac905fb4fbd4aa60cec74083079a5f2
PedroAS <85406215+PedroHAlvesS@users.noreply.github.com>
2022-06-01 23:17:46 -0300

    Resposta do exercicio 6
    As perguntas são adicionadas através do perguntas.txt

---
+++ Exercicio_6/src/Main.java
@@ -0,0 +1,29 @@
+import Domain.Perguntas;
+import Domain.Usuario;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Olá está pronto para um quiz ?");
+        Usuario usuario = new Usuario();
+        usuario.criarUsuario();
+        ArrayList<Perguntas> perguntas = Perguntas.lerPerguntasNoTxt();
+        for (Perguntas perguntaTemp : perguntas) {
+            perguntaTemp.lerPergunta();
+            System.out.println("Sr(a) " + usuario.getNome() + " informe o número da resposta que você acha correta: ");
+            int respostaUsuario = Integer.parseInt(teclado.nextLine());
+            if (perguntaTemp.acertouRespostaDaPergunta(respostaUsuario)) {
+                usuario.acertouPergunta();
+            } else {
+                usuario.errouPergunta();
+            }
+        }
+        usuario.lerResultadoDoUsuario();
+
+
+    }
+}
